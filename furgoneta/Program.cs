b2347ac692a39574486cddf3408bf923bea23a5e
DavidMunoz18 <145343117+DavidMunoz18@users.noreply.github.com>
2024-01-29 13:22:27 +0100

    Add files via upload

---
+++ furgoneta/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace furgoneta
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            
+                int carga;
+                int sacos;
+
+                carga = Convert.ToInt32(Console.ReadLine());
+
+                if (carga / 3 > 4000)
+                {
+
+                    sacos = 12000 / carga;
+
+                    Console.WriteLine(sacos / 3);
+
+                }
+                else
+                {
+                    Console.WriteLine("la carga es demasiado pesada para un solo camión");
+                }
+
+
+
+
+            
+        }
+    }
+}
